0fe4ec08b7c9ad19f1369c8b84b15f7013556e7d
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-05-23 19:56:28 +0100

    annotation add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+"""
+type-annotated function add that takes
+ a float a and a float b as arguments 
+ and returns their sum as a float
+"""
+
+
+def add(a: float, b: float):
+    """
+    function that reutrn the sum 
+    of a and b
+    """
+    return a + b
