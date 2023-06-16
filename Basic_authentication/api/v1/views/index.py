8642c0f9e915fea30236379749687552306356a5
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-06-16 09:38:09 +0100

    Basic authentication

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,9 +25,20 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route("/unauthorized", methods=['GET'], strict_slashes=False)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """
-    unauthorize request
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
+    abort(403)
