fb5868f74af7fc4515e9854f019139e768e3fe12
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-06-20 19:06:54 +0100

    hash pass

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,10 +3,36 @@
 define a _hash_password method
 """
 from bcrypt import hashpw, gensalt
-
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 def _hash_password(password: str) -> str:
-    """ 
-    Returns a salted hash of the input password 
+    """
+    Returns a salted hash of the input password
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers and returns a new user if email isn't listed
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
+        else:
+            raise ValueError(f'User {email} already exists')
+
+    
