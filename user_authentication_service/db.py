32e3b89edb46dc167a327e8e97aa106577b1bf4b
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-06-20 16:39:26 +0100

    SQLAlchemy model named User

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm.session import Session
 from user import Base
 from user import User
 
+
 class DB:
     """DB class
     """
@@ -29,6 +30,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds user to database
         Return: User Object
@@ -37,4 +39,3 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
-    
