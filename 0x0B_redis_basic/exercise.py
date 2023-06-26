d8afc8a2076321adde015be8213535d7d305dce7
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-06-26 09:53:07 +0100

    redis task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -25,3 +25,21 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get_str(self, key: str) -> str:
+        """
+        param a value from redis to str
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: int) -> int:
+        """
+        param a value from redis to int
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
