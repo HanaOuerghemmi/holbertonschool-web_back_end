91425a70fd718cb2790af3a5a9c85ff1ddfae4a7
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-06-26 12:00:42 +0100

    redis task 3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -10,67 +10,6 @@ from uuid import uuid4, UUID
 import redis
 
 
-class Cache:
-    """ Class for implementing a Cache """
-
-    def __init__(self):
-        """ Constructor Method """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store the input data in Redis using a
-        random key and return the key.
-        """
-        random_key = str(uuid4())
-        self._redis.set(random_key, data)
-
-        return random_key
-
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """
-        store input data in dedis using a random key
-        and return a key
-        """
-        value = self._redis.get(key)
-
-        return value
-
-    def get_str(self, key: str) -> str:
-        """
-        param a value from redis to str
-        """
-        value = self._redis.get(key)
-        return value.decode('utf-8')
-
-    def get_int(self, key: int) -> int:
-        """
-        param a value from redis to int
-        """
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode('utf-8'))
-        except Exception:
-            value = 0
-        return value
-
-
-def count_calls(method: Callable) -> Callable:
-    """ Decortator for counting how many times a function
-    has been called """
-
-    key = method.__qualname__
-
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """ Wrapper for decorator functionality """
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-
-    return wrapper
-
-
 def call_history(method: Callable) -> Callable:
     """
     store the history of inputs and
@@ -120,3 +59,66 @@ def replay(fn: Callable):
             o = ""
 
         print(f'{f_name}(*{i}) -> {o}')
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decortator for counting how many times a function
+    has been called """
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper for decorator functionality """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+class Cache:
+    """ Class for implementing a Cache """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the input data in Redis using a
+        random key and return the key.
+        """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        store input data in dedis using a random key
+        and return a key
+        """
+        value = self._redis.get(key)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        param a value from redis to str
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: int) -> int:
+        """
+        param a value from redis to int
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
