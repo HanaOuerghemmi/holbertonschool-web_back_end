f5cf0383962db6ed90f3406706c09b3b5944aed8
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-06-27 10:31:16 +0100

    valid email

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- SQL script that creates a trigger that resets the attribute valid_email
+-- only when the email has been changed.
+
+CREATE TRIGGER reset_valid_email_trigger BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+        -- Reset the valid_email attribute
+    END IF;
+END;
