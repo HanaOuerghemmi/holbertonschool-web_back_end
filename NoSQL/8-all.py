edb23221d4593447286ec849e4cf1bb4201ac413
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-07-06 10:50:15 +0100

    list

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """
+Python function that lists all
+documents in a collection:
 """
 
 
 def list_all(mongo_collection):
-    """*"""
+    """
+    Return an empty list if no document in the collection
+    """
     documents = mongo_collection.find()
     if documents.count() == 0:
         return []
