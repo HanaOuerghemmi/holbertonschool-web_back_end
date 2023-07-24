30924f480b161f99d56fb419027cdabeb4cb28e1
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-07-24 12:29:51 +0100

    ist std ids

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 export default function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-  
-    const ids = students.map((item) => item.id);
-  
-    return ids;
+  if (!Array.isArray(students)) {
+    return [];
   }
+
+  const ids = students.map((item) => item.id);
+
+  return ids;
+}
