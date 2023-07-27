75ff05fb0b0ffe9f1fe39427d40c2b1cfb64d9f2
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-07-27 18:28:59 +0100

    list

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,7 @@ export default function cleanSet(set, startString) {
   const string = [];
 
   if (
-    typeof set !== 'object' 
-    || typeof startString !== 'string' 
-    || startString.length === 0
+    typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0
   ) {
     return '';
   }
