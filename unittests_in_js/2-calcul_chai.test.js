d593b308f87ac483d44f829a1ecf7f756bc5f256
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-07-29 21:30:20 +0100

    basic test chai

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -13,5 +13,19 @@ describe('calculateNumber', function () {
       chai.expect(calculateNumber('SUM', 1, 4)).to.equal(5);
     });
   });
-
+  describe('SUBTRACT', function () {
+    it('should return 2', function () {
+      chai.expect(calculateNumber('SUBTRACT', 5, 3)).to.equal(2);
+    });
+  });
+  describe('DIVIDE', function () {
+    it('should return 2', function () {
+      chai.expect(calculateNumber('DIVIDE', 8, 4)).to.equal(2);
+    });
+  });
+  describe('DIVIDE Error', function () {
+    it('should return Error', function () {
+      chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
 });
