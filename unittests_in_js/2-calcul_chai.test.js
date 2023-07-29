c5f83cf064cb1d3b19da43e818b965577f2d4b64
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-07-29 21:34:10 +0100

    basic test chai

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -2,11 +2,6 @@ var chai = require('chai');
 
 const calculateNumber = require('./2-calcul_chai');
 
-const SUM = 'SUM';
-const SUBTRACT = 'SUBTRACT';
-const DIVIDE = 'DIVIDE';
-const INVALID = 'INVALID';
-
 describe('calculateNumber', function () {
   describe('SUM no Round', function () {
     it('should return 5', function () {
